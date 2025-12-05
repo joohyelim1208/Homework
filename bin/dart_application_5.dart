f5e31ddad5175ebc1fadbffc768e63249699d632
주혜림 <joohyelim@juhyelim-ui-MacBookAir.local>
2025-12-04 18:55:57 -0800

    필수문제 2번

--- bin/dart_application_5.dart
+++ bin/dart_application_5.dart
@@ -1,29 +1,5 @@
 import 'package:args/args.dart';
 
-// 1번 문제: 이 학생의 점수는 84점이며, 등급은 B등급 입니다! 1번째 문제 풀이 시도
-/*
-void main() {
-  // 1차 풀이시도
-  // 학생과 점수. 이렇게 학생 개개인별 부여 안해도 됨
-  Map<String, int> Students = {'Anna': 30, 'Bob': 84, 'Casey': 96};
-
-  // 등급을 정하는 함수
-  String grade (int score) {
-    if (score >= 90) {
-      return 'A';
-    } else if (score >= 80) {
-      return 'B';
-    } else {
-      return 'C';
-    }
-  }
-
-  // 학생의 점수와 등급을 출력
-  Students.
-  print ('이 $Students 학생의 점수는 $score점 이며, 등급은 $grade등급 입니다!');
-}
-*/
-
 void main() {
   // 1번 문제: 이 학생의 점수는 84점이며, 등급은 B등급 입니다!
   print('필수문제 1번');
